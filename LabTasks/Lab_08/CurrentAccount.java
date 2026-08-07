476bce10f86b08fdb32a20f7ded8fb23546d43f7
farhanniamatali171-creator <farhanniamatali171@gmail.com>
2026-08-07 09:37:56 +0500

    Create CurrentAccount.java

---
+++ LabTasks/Lab_08/CurrentAccount.java
@@ -0,0 +1,23 @@
+class CurrentAccount extends Bank_Account {
+    public CurrentAccount(int accountID, double balance) {
+        super(accountID, balance);
+    }
+
+    void amountWithdraw(double amount) {
+        if ((balance - amount) >= 5000) {
+            balance -= amount;
+            System.out.println("Withdraw Successfully!");
+        } else {
+            System.out.println("Withdraw not possible");
+        }
+    }
+
+    void amountDeposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposit Successfully!");
+        } else {
+            System.out.println("Invalid amount!");
+        }
+    }
+}
